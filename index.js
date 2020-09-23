fa99ac3d1d017eb9a1772fcea77d03eca3d9e960
GimpFlamingo <rayjkochenderfer@gmail.com>
2020-09-22 20:26:20 -0400

    Add basic functionality

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('guildMemberAdd', member => {
+	// Send the message to a designated channel on a server:
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+	// Do nothing if the channel wasn't found on this server
+	if (!channel) return;
+	// Send the message, mentioning the member
+	channel.send(`Welcome to the server, ${member}`);
+});
+
+// When user says the message "!ping"
+client.on('message', msg => {
+	if (msg.content === '!ping') {
+		msg.reply('Pong!')
+	}
+})
+
+client.login(process.env.TOKEN)
