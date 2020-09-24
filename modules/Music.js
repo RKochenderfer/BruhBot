5efc645c8bee6f1906332d4213abc42634809b8d
Ray Kochenderfer <35624960+GimpFlamingo@users.noreply.github.com>
2020-09-24 07:52:47 -0400

    Add Clip of leftist ass

--- modules/Music.js
+++ modules/Music.js
@@ -22,12 +22,20 @@ class Music {
 		if (permission === null) return false
 		try {
 			const conn = await voiceChannel.join()
-			const dispatcher = conn
-				.play(ytdl(song.url))
+			const dispatcer = conn
+				.play('C:\\Users\\rayjk\\repos\\BruhBot\\songs\\leftist_ass.mp3')
 				.on('finish', () => {
 					voiceChannel.leave()
 				})
-				.on('error', error => console.error(error))
+				.on('error', err => console.error(err))
+			/* This will play a youtube video and should be saved but switching to a .mp3 for clips */
+			// const conn = await voiceChannel.join()
+			// const dispatcher = conn
+			// 	.play(ytdl(song.url))
+			// 	.on('finish', () => {
+			// 		voiceChannel.leave()
+			// 	})
+			// 	.on('error', error => console.error(error))
 		} catch (err) {
 			console.log(err)
 			return false
