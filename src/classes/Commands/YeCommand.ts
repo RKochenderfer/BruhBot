0c3e57482162494ebbac8d04da7840302f78918e
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2021-09-07 15:33:53 -0400

    Implement ye command

--- src/classes/Commands/YeCommand.ts
+++ src/classes/Commands/YeCommand.ts
@@ -1,11 +1,16 @@
 import { CommandInteraction, Message } from 'discord.js'
 import { Command } from './Command'
 
+const fetch = require('node-fetch')
+
 export class YeCommand extends Command {
 	constructor() {
 		super('ye', 'Gets a random Kanye West quote')
 	}
 	async execute(interaction: CommandInteraction) {
-		interaction.reply({content: 'This has not been implemented it'})
+		const res = await fetch('https://api.kanye.rest')
+		const json = await res.json()
+
+		interaction.reply({content: `Kanye West says: "${await json.quote}""`})
 	}
 }
