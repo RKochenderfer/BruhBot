d1c23e0d8a76658d04a03c8b8ff63578749da690
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2022-03-24 22:46:44 -0400

    Create display table

--- src/classes/Commands/DiceRollerCommand.ts
+++ src/classes/Commands/DiceRollerCommand.ts
@@ -5,8 +5,111 @@ import { Command } from './Command'
 export class RollCommand extends Command {
 	private static dieCountName = 'count'
 	private static diceTypeName = 'type'
+
 	constructor() {
 		super('roll', 'rolls the specified die and the number to be rolled')
+	}
+
+	private getMidpointWhitespace(x: number, y: number): string {
+		let whitespace = ''
+		for (let i = 0; i < Math.floor((x - y) / 2); i++) {
+			whitespace += ' '
+		}
+		return whitespace
+	}
+
+	private padWhitespace(currentLength: number, endLength: number) {
+		let built = ''
+
+		for (let i = currentLength; i < endLength; i++) {
+			built += ' '
+		}
+
+		return built
+	}
+
+	private buildRollInfo(dieCount: number, dieType: number): string {
+		// todo: test this
+		let built = ``
+		const test = `
+		===========================
+		| Dice Rolled | Dice Type |
+		| ----------- | --------- |
+		|     10      |    d6     |
+		===========================
+		`
+		const outSideBar = '==========================='
+		const tableHeader = `
+		| Dice Rolled | Dice Type |
+		| ----------- | --------- |
+		`
+		const diceRolledCharLength = dieCount.toString().length
+		const dieTypeCharLength = dieType.toString().length
+		const dieRolledCellLength = 13
+		const diceTypeCellLength = 11
+
+		const firstPartDiceRolledWithWhitespace = `${this.getMidpointWhitespace(
+			dieRolledCellLength,
+			diceRolledCharLength,
+		)}${dieCount}`
+		const diceRolledWithWhitespace = `${firstPartDiceRolledWithWhitespace}${this.padWhitespace(
+			firstPartDiceRolledWithWhitespace.length,
+			dieRolledCellLength,
+		)}`
+
+		const firstPartDiceTypeWithWhitespace = `${this.getMidpointWhitespace(
+			diceTypeCellLength,
+			dieTypeCharLength + 1,
+		)}d${dieType}`
+		const diceTypeWithWhitespace = `${firstPartDiceTypeWithWhitespace}${this.padWhitespace(
+			firstPartDiceTypeWithWhitespace.length,
+			diceTypeCellLength,
+		)}`
+
+		built = `
+		${outSideBar}
+		${diceRolledWithWhitespace}|${diceTypeWithWhitespace}
+		${outSideBar}
+		`
+
+		return built
+	}
+
+	private rollResultsTable(vals: number[]): string {
+		let rollPresentation = `YOUR ROLLS: ${vals}`
+		let total = `YOUR TOTAL: ${vals.reduce((accumVal, curVal) => {
+			return accumVal + curVal
+		}, 0)}`
+		let border = ''
+
+		for (let i = 0; i < total.length; i++) {
+			border += '='
+		}
+
+		let built = `
+		${border}
+		${rollPresentation}
+		${border}
+		${total}
+		${border}
+		`
+
+		return built
+	}
+
+	private displayRoll(
+		dieCount: number,
+		dieType: number,
+		values: number[],
+	): string {
+		const rollInfoTable = this.buildRollInfo(dieCount, dieType)
+
+		const rollResultsTable = this.rollResultsTable(values)
+		
+		return `
+		${rollInfoTable}
+		${rollResultsTable}
+		`
 	}
 
 	buildCommand(): SlashCommandBuilder {
@@ -52,6 +155,6 @@ export class RollCommand extends Command {
 			values.push(this.getRandomInt(dieType))
 		}
 
-		interaction.reply({ content: `You rolled: ${values}` })
+		interaction.reply({ content: `${this.displayRoll(dieCount, dieType, values)}` })
 	}
 }
