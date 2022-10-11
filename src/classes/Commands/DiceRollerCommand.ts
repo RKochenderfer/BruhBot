f503a2a7951ba1ac67f385dd01af2ab3cb51a6d4
Raymond Kochenderfer <ray@pop-os.localdomain>
2022-10-11 10:53:28 -0400

    Add modifier string evaluation

--- src/classes/Commands/DiceRollerCommand.ts
+++ src/classes/Commands/DiceRollerCommand.ts
@@ -2,12 +2,14 @@ import { SlashCommandBuilder } from '@discordjs/builders'
 import { CommandInteraction } from 'discord.js'
 import { Command } from './Command'
 import { AsciiTable } from '../AsciiTable'
+import { Parser } from 'expr-eval'
 
 export class RollCommand extends Command {
 	private static dieCountName = 'count'
 	private static diceTypeName = 'type'
 	private static rollTypeName = 'roll'
 	private static isWhisperName = 'whisper'
+	private static parser = new Parser()
 
 	constructor() {
 		super('roll', 'rolls the specified die and the number to be rolled')
@@ -16,24 +18,17 @@ export class RollCommand extends Command {
 	private displayRoll(
 		dieCount: number,
 		dieType: number,
-		modifier: number,
+		modifier: string,
 		values: number[],
 	): string {
-		let rollEntry = ''
-		if (modifier > 0) {
-			rollEntry = `${dieCount}d${dieType}+${modifier}`
-		} else if (modifier < 0) {
-			rollEntry = `${dieCount}d${dieType}${modifier}`
-		} else {
-			rollEntry = `${dieCount}d${dieType}`
-		}
+		let rollEntry = `${dieCount}d${dieType}${modifier}`
 
 		const data = [
 			['Roll', 'Values', 'Total'],
 			[
 				rollEntry,
 				values.toString(),
-				values.reduce((prev, curr) => prev + curr, 0) + modifier,
+				values.reduce((prev, curr) => prev + curr, 0) + RollCommand.parser.evaluate(modifier),
 			],
 		]
 		const asciiTable = new AsciiTable('Roll Info')
@@ -66,6 +61,7 @@ export class RollCommand extends Command {
 	}
 
 	async execute(interaction: CommandInteraction) {
+		let values = []
 		const regex = /^\d+d\d+([+|-]\d)?/
 
 		const rollString = interaction.options.getString(
@@ -84,31 +80,37 @@ export class RollCommand extends Command {
 		let split = rollString.split('d')
 		let dieType = 0
 		let modifier = 0
-
-		if (split[1].includes('+')) {
-			const modSplit = split[1].split('+')
-			dieType = Number.parseInt(modSplit[0])
-			modifier = Number.parseInt(modSplit[1])
-		} else if (split[1].includes('-')) {
-			const modSplit = split[1].split('-')
-			dieType = Number.parseInt(modSplit[0])
-			modifier = -1 * Number.parseInt(modSplit[1])
-		} else {
-			dieType = Number.parseInt(split[1])
-		}
+		let modString = ''
+		const additionIndex = split[1].indexOf('+')
+		const subtractionIndex = split[1].indexOf('-')
 		const dieCount = Number.parseInt(split[0])
 
-		let values = []
+		let firstIndex = -1
+		if (additionIndex < 0 && subtractionIndex > 0) {
+			firstIndex = subtractionIndex
+		} else if (additionIndex > 0 && subtractionIndex < 0) {
+			firstIndex = additionIndex
+		} else {
+			firstIndex = additionIndex < subtractionIndex ? additionIndex : subtractionIndex
+		}
+
+		if (firstIndex === -1) {
+			dieType = Number.parseInt(split[1])
+		} else {
+			modString = split[1].substring(firstIndex)
+			dieType = Number.parseInt(split[1].substring(0, firstIndex))
+		}
+		
 		for (let i = 0; i < dieCount; i++) {
 			values.push(this.getRandomInt(dieType))
 		}
 
 		console.log(
-			`Die Count: ${dieCount}\nDie Type: ${dieType}\nModifier: ${modifier}\nValues: ${values}`,
+			`--------------\n*ROLL COMMAND*\nDie Count: ${dieCount}\nDie Type: ${dieType}\nModifier: ${modString}\nValues: ${values}\n--------------`,
 		)
 
 		interaction.reply({
-			content: this.displayRoll(dieCount, dieType, modifier, values),
+			content: this.displayRoll(dieCount, dieType, modString, values),
 			ephemeral: isWhisper ? true : false,
 		})
 	}
