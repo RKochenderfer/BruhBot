79a227d1023e2d0560e0a1f357686e2d30dd6664
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2023-02-05 20:19:02 -0500

    More changes

--- chat_trainer/chat_trainer.py
+++ chat_trainer/chat_trainer.py
@@ -6,6 +6,7 @@ import spacy
 
 # Import modules for training
 from rasa_nlu.converters import load_data
+from rasa.nlu.config import RasaNLUModelConfig
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
 
