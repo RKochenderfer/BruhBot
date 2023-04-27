9f854dd49d48020cfd3ad44f2e98c1344251f8b8
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2023-04-27 12:58:49 -0400

    Cleanup code

--- src/render.ts
+++ src/render.ts
@@ -5,52 +5,22 @@ import fs from 'fs'
 class MessageInfo {
 	constructor(public readonly user_name: string, public readonly text_content: string) { }
 }
-
+// TODO: Setup queue
 export let render = async (message: Message) => {
 	const num = validate(message)
 	if (num === null) return
-
-	const repliedTo = await message.channel.messages.fetch(
-		message.reference?.messageId!,
-	)
-
-	let messagesBefore = await message.channel.messages.fetch({
-		before: repliedTo.id,
-		limit: num - 1,
-	})
-
-	const messages: Array<MessageInfo> = [new MessageInfo(repliedTo.author.username, repliedTo.content)]
-	messagesBefore.forEach((value, key) => {
-		messages.push(new MessageInfo(value.author.username, value.content))
-	})
-
-	const date = new Date()
-	const year = date.getUTCFullYear()
-	const month = date.getUTCMonth() + 1
-	const day = date.getUTCDate()
-	const hour = date.getUTCHours()
-	const minute = date.getUTCMinutes()
-	const second = date.getUTCSeconds()
-	const fileName = `${year}-${month}-${day}T${hour}-${minute}-${second}.mp4`
+	const fileName = getFileName()
 
 	try {
-		const body = JSON.stringify({ 'file_name': fileName, 'messages': messages.reverse() })
-		console.log('Requesting')
+		const body = await buildRequestBody(message, num, fileName)
+		// console.log('Requesting')
 		const res = await fetch('http://objection-engine:5000/', {
 			method: 'POST',
 			body: body,
 		})
 
 		if (res.ok) {
-			const stream = fs.createReadStream(`output/${fileName}`)
-			stream.on('error', (err) => {
-				console.error(err)
-			})
-			const file = new AttachmentBuilder(stream)
-			await message.reply({
-				content: 'your video has been rendered',
-				files: [file],
-			})
+			await sendRenderedFile(fileName, message)
 		} else {
 			throw new Error(`Request failed: ${res}`)
 		}
@@ -66,14 +36,57 @@ let validate = (message: Message): number | null => {
 		return null
 	}
 
-	const num = message.content.trim().replace(/\D/g, '')
+	const num = message.content.trim().replace(/\D/g, '') // Removes everything except the the number
 	
 	const parsed = Number.parseInt(num)
 	if (parsed > 20) {
 		message.reply('The number must be less than 20')
-		console.log(`########## more than 20 ##########`)
 		return null
 	}
 
 	return parsed
+}
+
+let getFileName = () => {
+	const date = new Date()
+	const year = date.getUTCFullYear()
+	const month = date.getUTCMonth() + 1
+	const day = date.getUTCDate()
+	const hour = date.getUTCHours()
+	const minute = date.getUTCMinutes()
+	const second = date.getUTCSeconds()
+	return `${year}-${month}-${day}T${hour}-${minute}-${second}.mp4`
+}
+
+let buildRequestBody = async (message: Message, num: number, fileName: string) => {
+	// Get the message that the caller replied to
+	const repliedTo = await message.channel.messages.fetch(
+		message.reference?.messageId!,
+	)
+	// gather a number of messages that the sender requested
+	let messagesBefore = await message.channel.messages.fetch({
+		before: repliedTo.id,
+		limit: num - 1, // The -1 is so that the message they replied to is included and the total rendered matches the request
+	})
+
+	const messages: Array<MessageInfo> = [
+		new MessageInfo(repliedTo.author.username, repliedTo.content),
+	]
+	messagesBefore.forEach((value, key) => {
+		messages.push(new MessageInfo(value.author.username, value.content))
+	})
+
+	return JSON.stringify({ file_name: fileName, messages: messages.reverse() })
+}
+
+let sendRenderedFile = async (fileName: string, message: Message<boolean>) => {
+	const stream = fs.createReadStream(`output/${fileName}`) // Read the file from the shared output directory with the objection-engine
+	stream.on('error', (err) => {
+		console.error(err)
+	})
+	const file = new AttachmentBuilder(stream)
+	await message.reply({
+		content: 'your video has been rendered',
+		files: [file],
+	})
 }
