48bf943d86f6c6f5b6b3868432f4b0e4a40a7b1e
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2024-04-02 20:59:26 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{env, path::Path};
+use anyhow::{Error, Result};
+use bruhbot::{self, models::user_data::UserData};
 use dotenvy;
-use log::info;
-use anyhow::Result;
-use poise::serenity_prelude::{self as serenity, GatewayIntents};
-use bruhbot;
+use log::{error, info, warn};
+use poise::{framework, serenity_prelude::{self as serenity, GatewayIntents}};
+use std::{env, path::Path, sync::Arc, time::Duration, vec};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -23,12 +23,98 @@ fn load_env() -> Result<()> {
         let provided_env_file_path = &args[1];
         dotenvy::from_path(Path::new(provided_env_file_path))?;
         info!("Env file path: {} provided.", provided_env_file_path);
-    }
-    else {
-        println!("No arguments provided");
+    } else {
         info!("No env file provided. Using current environment variables");
     }
+
+    let options = poise::FrameworkOptions {
+        commands: vec![bruhbot::commands::bruh::bruh()],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("~".into()),
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            additional_prefixes: vec![
+                poise::Prefix::Literal("hey bot"),
+                poise::Prefix::Literal("hey bot,"),
+            ],
+            ..Default::default()
+        },
+        // The global error handler for all error cases that may occur
+        on_error: |error| Box::pin(on_error(error)),
+        // This code is run before every command
+        pre_command: |ctx| {
+            Box::pin(async move {
+                info!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        // This code is run after a command if it was successful (returned Ok)
+        post_command: |ctx| {
+            Box::pin(async move {
+                info!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+        // Every command invocation must pass this check to continue execution
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.author().id == 123456789 {
+                    return Ok(false);
+                }
+                Ok(true)
+            })
+        }),
+        // Enforce command checks even for owners (enforced by default)
+        // Set to true to bypass checks, which is useful for testing
+        skip_checks_for_owners: false,
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!(
+                    "Got an event in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                println!("Logged in as {}", _ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(UserData {
+                    votes: Mutex::new(HashMap::new()),
+                })
+            })
+        })
+        .options(options)
+        .build();
+
+    let token = var("DISCORD_TOKEN")
+        .expect("Missing `DISCORD_TOKEN` env var, see README for more information.");
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap()
 
     Ok(())
 }
 
+async fn on_error(error: poise::FrameworkError<'_, UserData, Error>) {
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx, .. } => {
+            error!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                error!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
