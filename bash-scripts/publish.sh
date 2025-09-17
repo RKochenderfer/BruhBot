c690fe4319ac7f65d94c80b6710c3050aa39284e
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2025-09-17 11:09:49 -0400

    Update publishing to use version from package.json

--- bash-scripts/publish.sh
+++ bash-scripts/publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd "$(dirname $0)" 
 cd ../
+# Read version from package.json
+VERSION=$(grep '"version"' ./package.json | head -1 | sed -E 's/.*"version": *"([^"]+)".*/\1/')
 
-docker build -t ghcr.io/rkochenderfer/bruhbot:latest . --load && docker push ghcr.io/rkochenderfer/bruhbot:latest
+echo "Detected version: $VERSION"
+
+echo "Building Bruhbot"
+docker build -t ghcr.io/rkochenderfer/bruhbot:"$VERSION" . --load
+docker push ghcr.io/rkochenderfer/bruhbot:"$VERSION"
